e546028dfacffcc1eea68cf7dff7818a99ee351d
andrewzhen <ajzhen@ucsd.edu>
2021-01-18 15:57:56 -0800

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,506 @@
+var data = {
+  "links" : [
+    // Copy and paste this to add a new event (order matters!)
+    // {
+    //   "title" : "",
+    //   "logo" : "",
+    //   "url" : ""
+    // },
+
+    {
+      "title": "Instagram photo contest rules",
+      "logo": "instagram.svg",
+      "url": "http://tritonfest.ucsd.edu/goodcleanfun/contest"
+    },
+    {
+      "title": "Triton Fest Best Playlist",
+      "logo": "spotify.svg",
+      "url": "https://open.spotify.com/playlist/56hTRc6b9aJrXmiQPv8BeZ?si=fmOHj17ATe-Z0m--IQ23GA"
+    },
+    {
+      "title": "ASCE Live with Chris Klemens",
+      "logo": "asce.svg",
+      "url": "https://www.facebook.com/events/612094943056885/"
+    },
+    {
+      "title": "Tiny Stream Concert",
+      "logo": "asce.svg",
+      "url": "https://www.facebook.com/events/543767189645412/"
+    },
+    {
+      "title": "How To Read Music Sheets",
+      "logo": "zone.svg",
+      "url": "https://www.facebook.com/events/637463743505445/"
+    },
+    {
+      "title": "Buzzfeed's 27 Zero Waste DIY Ideas",
+      "logo": "diy.svg",
+      "url": "https://www.buzzfeed.com/peggy/zero-waste-diys?ref=hpsplash"
+    },
+    {
+      "title": "Antoni Porowski's Queer Eye (cooking demos)",
+      "logo": "qe.svg",
+      "url": "https://www.instagram.com/antoni/channel/"
+    },
+    {
+      "title": "Disney Parks Churro Bites",
+      "logo": "disney.svg",
+      "url": "https://disneyparks.disney.go.com/blog/2020/04/cooking-up-the-magic-create-disneymagicmoments-at-home-with-disney-parks-churro-bites-recipe/"
+    },
+    {
+      "title": "Free Digital Library With Libby App",
+      "logo": "libby.svg",
+      "url": "https://www.overdrive.com/apps/libby/"
+    },
+    {
+      "title": "Learn Korean with BTS",
+      "logo": "bts.svg",
+      "url": "https://metro.co.uk/2020/03/24/learn-korean-bts-phrases-korean-alphabet-12446986/"
+    },
+    {
+      "title": "Zoom background download",
+      "logo": "zoom.svg",
+      "url": "http://tritonfest.ucsd.edu/goodcleanfun/assets/images/TF2020_Zoom.png"
+    },
+    {
+      "title": "Archive",
+      "logo": "none",
+      "url": "http://tritonfest.ucsd.edu/goodcleanfun/archive"
+    }
+  ]
+}
+
+var archive = {
+  "Week 8" : [
+    {
+      "title" : "7-Day Free Trial to Stream Broadway Productions",
+      "logo" : "broadwayhd.svg",
+      "url" : "https://www.broadwayhd.com/"
+    },
+    {
+      "title" : "So... Over COVID Livestream Music Fest",
+      "logo" : "ocmusicfestival.svg",
+      "url" : "https://www.oc-musicfest.com/"
+    },
+    {
+      "title" : "Cast of Community Reunites - Q&A",
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/watch?v=pv9xMs2pU5g"
+    },
+    {
+      "title" : "The Loft Radio Hour: Moontower",
+      "logo" : "theloft.svg",
+      "url" : "https://www.facebook.com/events/699223864170535/"
+    },
+    {
+      "title" : "Free Guitar Lessons with Fender",
+      "logo" : "fender.svg",
+      "url" : "https://try.fender.com/play/playthrough/?banner=PLAY_20200323.PlayThrough2020.Pencil.Shop"
+    },
+    {
+      "title" : "Pancake Cereal TikTok Recipe",
+      "logo" : "tiktok.svg",
+      "url" : "https://www.refinery29.com/en-us/2020/05/9765349/pancake-cereal-tiktok-recipe"
+    },
+    {
+      "title" : "SCSC x CoBoard Presents: Digital Kunchocshun with AJ Rafael",
+      "logo" : "sixth.svg",
+      "url" : "https://www.facebook.com/events/264797798248291/"
+    },
+    {
+      "title" : "ASCE Live with Binging with Babish",
+      "logo" : "asce.svg",
+      "url" : "https://www.facebook.com/events/232756401349673/"
+    }
+  ],
+
+  "Week 7" : [
+    {
+      "title" : "Online Well-Being Programs",
+      "logo" : "caps.svg",
+      "url" : "https://wellness.ucsd.edu/CAPS/programs/Pages/may.aspx"
+    },
+    {
+      "title" : "14th Annual Asian and Pacific Islander American Heritage Celebration Month",
+      "logo" : "pan.svg",
+      "url" : "https://blink.ucsd.edu/HR/policies/diversity/APIA.html"
+    },
+    {
+      "title" : "Commencement the Podcast",
+      "logo" : "iheartradio.svg",
+      "url" : "https://www.iheart.com/podcast/1119-commencement-speeches-for-62117327/?utm_source=Facebook&utm_medium=CPM&utm_campaign=FLNA_2020_Doritos_Graduation"
+    },
+    {
+      "title" : "Facebook/Instagram Livestream Commencement",
+      "logo" : "fb.svg",
+      "url" : "https://www.facebook.com/facebookapp/videos/378060546441584/"
+    },
+    {
+      "title" : "Kanopy - Free Streaming Platform",
+      "logo" : "kanopy.svg",
+      "url" : "https://www.kanopy.com/"
+    },
+    {
+      "title" : "Gucci Osteria's Massimo Bottura Free IG Cooking Classes",
+      "logo" : "instagram.svg",
+      "url" : "https://www.instagram.com/massimobottura/"
+    },
+    {
+      "title" : "Elevate: Commerce (Commerce/marketing workshop)",
+      "logo" : "adweek.svg",
+      "url" : "https://www.adweek.com/events-and-awards/adweek-elevate-commerce/"
+    },
+    {
+      "title" : "Call of Duty: Warzone",
+      "logo" : "cod.svg",
+      "url" : "https://www.cnet.com/news/call-of-duty-warzone-cheaters-punished-new-modes-and-everything-else-you-need-to-know/"
+    }
+  ],
+
+  "Week 6" : [
+    {
+      "title" : "ASCE Live with Steven Lim",
+      "logo" : "asce.svg",
+      "url" : "https://www.facebook.com/events/273164343815010/"
+    },
+    {
+      "title" : "Dryland Surf Training",
+      "logo" : "triton.svg",
+      "url" : "https://www.youtube.com/watch?v=R3O1EeZl4lI&list=PL-UI5KvgX1XCml9V1L7dc0t1T0clPk7CR&index=32&t=0s"
+    },
+    {
+      "title" : "Porter Robinson's Secret Sky Festival",
+      "logo" : "robinson.svg",
+      "url" : "https://www.secretskyfest.com/"
+    },
+    {
+      "title" : "Some Good News Graduation",
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/watch?v=IweS2CPSnbI"
+    },
+    {
+      "title" : "San Diego Zoo Animal Webcams",
+      "logo" : "zoo.svg",
+      "url" : "https://zoo.sandiegozoo.org/live-cams"
+    },
+    {
+      "title" : "Cast Reading of Harry Potter Books",
+      "logo" : "hp.svg",
+      "url" : "https://www.wizardingworld.com/collections/harry-potter-at-home"
+    },
+    {
+      "title" : "Healthy No-Cook Meals",
+      "logo" : "triton.svg",
+      "url" : "https://recreation.ucsd.edu/2020/04/healthy-no-cook-meal-ideas/?fbclid=IwAR2Qf0uEzDxVeDK-77TIQJ9K1o9XPyG2rSkvG0PONlSp9ThIaVL9CK1tK4A"
+    },
+    {
+      "title" : "21 DIY Projects To Make While in Quarantine",
+      "logo" : "diy.svg",
+      "url" : "https://somethingturquoise.com/2020/03/18/quarantine-diy-projects/"
+    }
+  ],
+
+  "Week 5" : [
+    {
+      "title": "work/study from home survival tips by WITHWENDY",
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/watch?v=VJGMsDkHDG8"
+    },
+    {
+      "title" : "SXSW 2020 Film Festival Collection",
+      "logo" : "sxsw.svg",
+      "url" : "https://www.amazon.com/gp/video/storefront/ref=atv_me_sxs_c_UnTV89_cNV3vj_1_1?contentType=merch&contentId=SXSW&merchId=SXSW"
+    },
+    {
+      "title" : "Disney Drawing Classes",
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/playlist?list=PLflf8xbnbx65Z2oa2F9uqKZxJJn-4Nynr"
+    },
+    {
+      "title" : "Live Body Weight Bootcamp Class",
+      "logo" : "triton.svg",
+      "url" : "https://recreation.ucsd.edu/recreation-live/?fbclid=IwAR0bcQ5ks7r1IJNA517XlFjPNd3DsdsACRsKIet83tY9Z-Em_dpV5pMk2OM"
+    },
+    {
+      "title" : "Mug Meal Recipes",
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/watch?v=w77-0EZQ7Ec"
+    },
+    {
+      "title" : "Virtual Career Center & Advising",
+      "logo" : "career.svg",
+      "url" : "https://career.ucsd.edu/about/index.html"
+    },
+    {
+      "title" : "Love From Philly (May 1-3)",
+      "logo" : "bell.svg",
+      "url" : "https://lovefromphilly.live/"
+    },
+    {
+      "title" : "Guided Meditation with Diddy",
+      "logo" : "audible.svg",
+      "url" : "https://www.audible.com/pd/Honor-Yourself-Audiobook/B0877D7PGV"
+    }
+  ],
+
+  "Week 4" : [
+    {
+      "title": "10 Min Sunrise Yoga",
+      "logo": "youtube.svg",
+      "url": "https://www.youtube.com/watch?v=EoX3q0GQ_Xk&feature=youtu.be"
+    },
+    {
+      "title": "virtual iCafé – Passport to Culture Around the World!",
+      "logo": "cafe.svg",
+      "url": "icafe.ucsd.edu"
+    },
+    {
+      "title": "Muir Online & Remote Engagement",
+      "logo": "muir.svg",
+      "url": "https://muir.ucsd.edu/studentlife/more.html"
+    },
+    {
+      "title": "Tour the world's biggest museums",
+      "logo": "google.svg",
+      "url": "https://artsandculture.google.com/partner?hl=en"
+    },
+    {
+      "title": "Learn French or Spanish for free",
+      "logo": "duolingo.svg",
+      "url": "https://podcast.duolingo.com/"
+    },
+    {
+      "title": "Tik Tok Renegade Dance Tutorial",
+      "logo": "youtube.svg",
+      "url": "https://www.youtube.com/watch?v=Th9f7_KxMf4"
+    },
+    {
+      "title": "DIY Face (Covering) Masks with LaurDIY",
+      "logo": "youtube.svg",
+      "url": "https://www.youtube.com/watch?v=e5btJ0_KTU8"
+    },
+    {
+      "title": "Triton Transfers Virtual Meet Ups",
+      "logo": "transfer.svg",
+      "url": "https://www.instagram.com/tritontransfers/"
+    },
+    {
+      "title": "Michael Jordan Documentary 'The Last Dance'",
+      "logo": "espn.svg",
+      "url": "https://nba.nbcsports.com/2020/04/17/michael-jordan-documentary-how-to-watch/"
+    }
+  ],
+
+  "Week 3": [
+    {
+      "title": "Psych! Free multiplayer App",
+      "logo": "app.svg",
+      "url": "https://apps.apple.com/us/app/psych-outwit-your-friends/id1005765746"
+    },
+    {
+      "title": "Hidden Narratives of the Coronavirus with Steven Lim",
+      "logo": "spotify.svg",
+      "url": "https://open.spotify.com/show/20avqoSjSDRkvK3jEDaWyW"
+    },
+    {
+      "title": "Free guitar lessons for all levels",
+      "logo": "triton.svg",
+      "url": "https://recreation.ucsd.edu/recreation-live/"
+    },
+    {
+      "title": "Tigertail",
+      "logo": "netflix.svg",
+      "url": "https://www.netflix.com/title/80202958"
+    },
+    {
+      "title": "Free online courses from the Museum of Modern Art",
+      "logo": "coursera.svg",
+      "url": "https://www.coursera.org/moma"
+    },
+    {
+      "title": "500 Hours of Free HBO",
+      "logo": "hbo.svg",
+      "url": "https://www.hbo.com/hbo-news/watch-hbo-free-no-subscription"
+    },
+    {
+      "title": "Join an Esports spring league",
+      "logo": "triton.svg",
+      "url": "https://rec.ucsd.edu/DivisionRegistration"
+    },
+    {
+      "title": "Navigating Networking (April 21)",
+      "logo": "zone.svg",
+      "url": "https://wellness.ucsd.edu/zone/"
+    }
+  ],
+
+  "Week 2": [
+    {
+      "title" : '"The Playground"',
+      "logo" : "triton.svg",
+      "url" : "https://recreation.ucsd.edu/"
+    },
+    {
+      "title" : 'Coachella "20 Years in the Desert"',
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/watch?v=jjwilAja7Lc"
+    },
+    {
+      "title" : "Dalgona coffee at home",
+      "logo" : "youtube.svg",
+      "url" : "https://youtu.be/B2cZCl-Um_I"
+    },
+    {
+      "title" : "Some Good News with John Krasinski",
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/watch?v=F5pgG1M_h_U"
+    },
+    {
+      "title" : "Thinking of You Podcast",
+      "logo" : "spotify.svg",
+      "url" : "https://open.spotify.com/show/11oCEF3XB4Q7hGbQTL0U2I?si=AOF4RC8WQqa79NAXtN7D5A"
+    },
+    {
+      "title" : "Rex Orange County - Tiny Desk Concert",
+      "logo" : "npr.svg",
+      "url" : "https://www.youtube.com/watch?v=Y8xGS9eJQRg"
+    },
+    {
+      "title" : "10 simple self-care habits for staying calm & positive at home",
+      "logo" : "youtube.svg",
+      "url" : "https://www.youtube.com/watch?v=Iiw2pD3Xvpk"
+    },
+    {
+      "title" : "Empty San Diego",
+      "logo" : "utsd.svg",
+      "url" : "https://www.sandiegouniontribune.com/latest/gallery/photos-empty-san-diego"
+    }
+  ]
+}
+
+window.onload = function() {
+  // Load events into DOM
+  function loadEvent(lst, el) {
+    for (let i = 0; i < lst.length; i++ ) {
+      let a = document.createElement("a");
+      let div = document.createElement("div");
+      let h2 = document.createElement("h2");
+      let h2_text = document.createTextNode(lst[i].title);
+  
+      a.classList.add("link");
+      a.href = lst[i].url;
+  
+      let logo = lst[i].logo;
+      
+      if (logo != "none") {
+        div.style.backgroundImage = "url(./assets/images/" + logo;
+      }
+
+      if (logo != "instagram.svg" && logo != "none") {
+        a.target = "_blank";
+      } 
+      
+      if (logo == "zoom.svg") {
+        a.download = "TF2020_Zoom.png";
+      }
+  
+      div.classList.add("link-logo");
+      h2.classList.add("link-title")
+  
+      if (logo == "none") {
+        div.style.width = "0";
+        h2.style.width = "100%";
+      }
+  
+      h2.appendChild(h2_text);
+      a.appendChild(div);
+      a.appendChild(h2);
+
+      try {
+        el.appendChild(a);
+      } catch (error) {
+        if (!document.getElementById("content-container")) {
+          console.log("You are viewing the archived links.");  
+        } else {
+          console.log("You are viewing the most current links.");
+        }
+        break; 
+      }
+    }
+  }
+
+  function loadWeek(lst, el) {
+    for (let i = 0; i < Object.keys(lst).length; i++) {
+      let a = document.createElement("a");
+      let div = document.createElement("div");
+      let h2 = document.createElement("h2");
+      let h2_text = document.createTextNode(Object.keys(lst)[i]);
+      let url = Object.keys(lst)[i].replace(/\s/g, '').toLowerCase();
+  
+      a.classList.add("link");
+      a.href = "http://tritonfest.ucsd.edu/goodcleanfun/" + url;
+
+      h2.appendChild(h2_text);
+      a.appendChild(div);
+      a.appendChild(h2);
+
+      try {
+        el.appendChild(a);
+      } catch (error) {
+        if (!document.getElementById("content-container")) {
+          console.log("You are viewing the archived links.");  
+        } else {
+          console.log("You are viewing the most current links.");
+        }
+        break; 
+      }
+    }
+  }
+
+  // Load events
+  loadEvent(data.links, document.getElementById("content-container"));
+  loadWeek(archive, document.getElementById("archive-container"));
+  for (let i = 0; i < Object.keys(archive).length; i++) {
+    loadEvent(archive['Week ' + (i + 2)], document.getElementById("week" + (i + 2) + "-container"));
+  }
+
+  // Calculate height
+  var D = document;
+  let total_height = Math.max(
+    Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+    Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+    Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+  )
+
+  try {
+    let arrow = document.getElementById("arrow");
+    arrow.addEventListener("click", function() {
+      // Pageyoffset changes
+      if (window.pageYOffset + window.innerHeight >= total_height) {
+        window.scrollTo(0, 0);
+      } else {
+        window.scrollTo(0, document.body.scrollHeight);
+      }
+
+      // Rotate arrow
+      arrow.classList.toggle("rotate-arrow");
+    })
+
+    // Rotate arrow on scroll if reached bottom
+    window.addEventListener("scroll", function() {
+      if (window.pageYOffset + window.innerHeight >= total_height) {
+        arrow.style.transform = "rotate(180deg)";
+      } else {
+        arrow.style.transform = "rotate(0deg)";
+      }
+    });
+  } catch (error) {}
+
+  try {
+    let arrowBack = document.getElementById("arrow-back");
+    arrowBack.style.transform = "rotate(90deg)";
+    arrowBack.addEventListener("click", function() {
+      window.history.back();
+    })
+  } catch (error) {}
+}
